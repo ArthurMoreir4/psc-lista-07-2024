5a57afb7b731de017446fa8795ea5754cd73f04d
Arthur <160737964+ArthurMoreir4@users.noreply.github.com>
2024-04-16 23:07:50 -0300

    atividade 2

--- Main (2).java
+++ Main (2).java
@@ -1,67 +1,39 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main
-{
+public class questionC {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> num = new ArrayList<>();
-        int valor;
-        int i = 0;
-        int soma = 0;
-        int media = 0;
-        boolean op = true;
-        System.out.println("Digite os valores de notas (-1 para sair)");
-        while (op = true) {
-            try {
-                valor = sc.nextInt();
-                if (valor == -1) {
-                    i--;
-                    op = false;
-                    break;
-                } else {
-                    num.add(valor);
-                    i++;
-                }
-            } catch(Exception e) {
-                System.out.println("Erro: Digite um valor válido");
-                sc.next();
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> perguntas = new ArrayList<>();
+        perguntas.add("Telefonou para a vítima?");
+        perguntas.add("Esteve no local do crime?");
+        perguntas.add("Mora perto da vítima?");
+        perguntas.add("Devia para a vítima?");
+        perguntas.add("Já trabalhou com a vítima?");
+
+        int respostasPositivas = 0;
+
+        for (String pergunta : perguntas) {
+            System.out.println(pergunta + " (Responda 'sim' ou 'não'):");
+            String resposta = scanner.nextLine().toLowerCase();
+
+            if (resposta.equals("sim")) {
+                respostasPositivas++;
             }
         }
-        System.out.println("Foram inseridos " + (i + 1) + " números");
-        System.out.println("Ordem Crescente:");
-        for (int l = 0; l <= i; l++) {
-            if (l == i) {
-                System.out.println(num.get(l));
-            } else {
-                System.out.print(num.get(l) + ", ");
-            }
+
+        if (respostasPositivas == 2) {
+            System.out.println("Classificação: Suspeita");
+        } else if (respostasPositivas >= 3 && respostasPositivas <= 4) {
+            System.out.println("Classificação: Cúmplice");
+        } else if (respostasPositivas == 5) {
+            System.out.println("Classificação: Assassino");
+        } else {
+            System.out.println("Classificação: Inocente");
         }
-        System.out.println("Ordem Decrescente:");
-        for (int l = i; l >= 0; l--) {
-            System.out.println(num.get(l));
-        }
-        System.out.println("Soma:");
-        for (int l = 0; l <= i; l++) {
-            soma += num.get(l);
-        }
-        System.out.println(soma);
-        System.out.println("Média");
-        media = (soma / (i +1));
-        System.out.println(media);
-        System.out.println("Acima da média");
-        for (int l = 0; l <= i; l++) {
-            if (num.get(l) > media) {
-                System.out.println(num.get(l));
-            }
-        }
-        System.out.println("Abaixo de 7");
-        for (int l = 0; l <= i; l++) {
-            if (num.get(l) < 7) {
-                System.out.println(num.get(l));
-            }
-        }
-        System.out.println("Mensagem");
-        sc.close();
+
+        scanner.close();
     }
 }
